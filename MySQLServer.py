5b9b52515c3d304cd794160c3257418cf93956a0
mr-robot-del <fenzyaboy4life@gmail.com>
2025-08-22 05:04:18 -0700

    My SQL Server

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,6 +1,5 @@
 import sys
 import mysql.connector
-from mysql.connector import Error
 
 if len(sys.argv) != 3:
     print("Usage: python MySQLServer.py <username> <password>")
@@ -18,13 +17,12 @@ try:
         user=username,
         password=password
     )
-    conn.get_warnings = True
     cursor = conn.cursor()
     cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
     warnings = cursor.fetchwarnings()
-    if warnings is None or len(warnings) == 0:
+    if warnings is None:
         print("Database 'alx_book_store' created successfully!")
-except Error as e:
+except mysql.connector.Error as e:
     print(f"Error: {e}")
 finally:
     if cursor is not None:
