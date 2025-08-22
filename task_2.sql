dbde3dd30c4de5af1e3fb5dcdca29bfd0ea40ab5
mr-robot-del <fenzyaboy4life@gmail.com>
2025-08-22 05:34:26 -0700

    alx books store task_2

--- task_2.sql
+++ task_2.sql
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS Order_Details (
     order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
-    quantity INT NOT NULL,
+    quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
